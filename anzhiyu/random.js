2ca64121d057623752729d33e19b0d1450c8e038
auroraisok <chen228001@foxmail.com>
2024-10-05 19:56:36 +0800

    Site updated: 2024-10-05 19:56:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/05/维护中/"];function toRandomPost(){
+var posts=["2024/06/25/island剧情分析/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
