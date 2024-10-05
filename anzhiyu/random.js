b72a7b8a78d8c6143ed905171e939017f49e5846
auroraisok <chen228001@foxmail.com>
2024-10-05 22:29:41 +0800

    Site updated: 2024-10-05 22:29:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/25/island剧情分析/"];function toRandomPost(){
+var posts=["2024/06/25/island剧情分析/","2024/10/05/【gal随写】樱之杜/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
