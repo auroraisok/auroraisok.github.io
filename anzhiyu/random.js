17edf5f464ec740172f42864d1e52b23220bd5a2
auroraisok <chen228001@foxmail.com>
2024-10-05 15:03:18 +0800

    Site updated: 2024-10-05 15:03:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/05/hello-world/"];function toRandomPost(){
+var posts=["2024/10/05/维护中/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
