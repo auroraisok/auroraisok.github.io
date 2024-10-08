2aaaa2dbd6a579b0b5ccb8a2f5d60c974a3525eb
auroraisok <chen228001@foxmail.com>
2024-10-08 17:58:32 +0800

    Site updated: 2024-10-08 17:58:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/25/island剧情分析/","2024/01/24/【1000-TB】Alist网盘/","2024/10/05/【gal随写】樱之杜/","2024/10/07/【前端学习1】Html知识总结/","2024/10/06/【新人向】给Unity初学者的建议/","2024/10/07/【前端学习零】前端介绍/","2024/10/08/【Unity1】Unity入门笔记/","2024/06/23/Narcissu小说/"];function toRandomPost(){
+var posts=["2024/06/25/island剧情分析/","2024/01/24/【1000-TB】Alist网盘/","2024/10/05/【gal随写】樱之杜/","2024/06/23/Narcissu小说/","2024/10/07/【前端学习1】Html知识总结/","2024/10/07/【前端学习零】前端介绍/","2024/10/08/【Unity1】Unity入门笔记/","2024/10/06/【新人向】给Unity初学者的建议/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
